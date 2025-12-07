747da1372ee637fa349939509757130405023c4e
Aleh Palishchuk <aleh.palishchuk@gmal.com>
2025-12-07 15:03:20 +0300

    feat: add account base page

---
+++ app/(pages)/(root_layout)/accounts/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { PageTitle } from '@/app/components/pageTitle/PageTitle';
+
+const Page = () => {
+  return (
+    <div>
+      <PageTitle>Счета</PageTitle>
+    </div>
+  );
+};
+
+export default Page;
